74b9babacc23fd40ab2578e5a17260a8eaba7b87
gözdenur <81879623+eyupoglugozdenur@users.noreply.github.com>
2021-11-14 20:35:06 +0300

    python
    Maximum Of Three Numbers

---
+++ MaximumNumber.py
@@ -0,0 +1,13 @@
+num1=int(input("Please Ente Number 1: "))
+num2=int(input("Please Ente Number 2: "))
+num3=int(input("Please Ente Number 3: "))
+
+max=num1
+
+if num2>max:
+    max=num2
+    
+if num3>max:
+    max=num3
+    
+print("Maximum Number Is:", max)
