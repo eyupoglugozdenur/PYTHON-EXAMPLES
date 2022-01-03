584cf313ddcb3395c5a08cfb31546f39a03f328f
gözdenur <81879623+eyupoglugozdenur@users.noreply.github.com>
2022-01-03 12:25:42 +0300

    python
     This program prints following figure
    
     
       Marry Christmas!
              *
             ***
            *****
           *******
          *********
         ***********
        *************
       ***************
      *****************
      *****************
       ***************
        *************
         ***********
          *********
           *******
            *****
             ***
              *
       Marry Christmas!

---
+++ figure1.py
@@ -0,0 +1,17 @@
+i=1
+j=9
+print(" ")
+print(" " *2,"Marry Christmas!")
+while i!=19:
+   print(" " *j,"*" *i)
+   i+=2
+   j-=1
+
+i=17
+j=1
+while i!=-1:
+    print(" " *j, "*" *i)
+    i-=2
+    j+=1
+print(" " *2,"Marry Christmas!")
+print(" ")
